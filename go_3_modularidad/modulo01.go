dbadb2ce760c597afaae9144e131bc7406ada6ae
ujuarez71 <64694328+ujuarez71@users.noreply.github.com>
2023-11-28 14:58:25 -0600

    Add files via upload

---
+++ go_3_modularidad/modulo01.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"omgo/go_3_modularidad/saludos"
+)
+
+func main() {
+	saludos.HolaMundo()
+}
---
+++ go_3_modularidad/saludos/saludos.go
@@ -0,0 +1,7 @@
+package saludos
+
+import "fmt"
+
+func HolaMundo() {
+	fmt.Println("Hola Mundo!")
+}
